dabd07526e5f361ffd3f65a32bc1b67d683d90c5
Junxian Li <88318195+lijunxian111@users.noreply.github.com>
2026-02-03 20:31:27 +0800

    Update seedream.py

--- model_eval/seedream.py
+++ model_eval/seedream.py
@@ -1,5 +1,4 @@
 import os
-# Install SDK:  pip install 'volcengine-python-sdk[ark]'
 from volcenginesdkarkruntime import Ark 
 import base64
 from PIL import Image
@@ -7,37 +6,16 @@ import json
 from io import BytesIO
 import mimetypes
 
-api_key = "5d1705be-9a72-4ade-a9a3-72410d720727"
+api_key = "xxxxxx"
 
 def save_base64_image_from_json(json_input, image_key='image_base64'):
-    # 解析 JSON 字符串（如果传入的是 str）
-    #print(type(json_input))
-    """
-    if isinstance(json_input, str):
-        try:
-            data = json.loads(json_input)
-        except json.JSONDecodeError:
-            raise ValueError("Invalid JSON string provided.")
-    elif isinstance(json_input, dict):
-        data = json_input
-    else:
-        raise TypeError("json_input must be a str or dict")
-    """
+   
     data = json_input
     b64_string = data
-    # 获取 base64 内容
-    """
-    if image_key not in data:
-        raise ValueError(f"Key '{image_key}' not found in JSON.")
     
-    b64_string = data[image_key]
-    """
-
-    # 如包含头部（data:image/png;base64,...），移除它
     if ',' in b64_string:
         b64_string = b64_string.split(',')[1]
 
-    # 解码并保存图像
     try:
         image_data = base64.b64decode(b64_string)
         image = Image.open(BytesIO(image_data)).convert("RGB")
@@ -48,15 +26,13 @@ def save_base64_image_from_json(json_input, image_key='image_base64'):
 def encode_file(file_path):
     mime_type, _ = mimetypes.guess_type(file_path)
     if not mime_type or not mime_type.startswith("image/"):
-        raise ValueError("不支持或无法识别的图像格式")
+        raise ValueError("Not supported")
     with open(file_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
     return f"data:{mime_type};base64,{encoded_string}"
 
 client = Ark(
-    # The base URL for model invocation
     base_url="https://ark.cn-beijing.volces.com/api/v3", 
-    # Get API Key：https://console.volcengine.com/ark/region:ark+cn-beijing/apikey
     api_key=api_key, 
 )
  
