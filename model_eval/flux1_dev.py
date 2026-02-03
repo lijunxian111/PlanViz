8b70c689de397ecbd8c3ce0dfd5e78b0594bcb26
Junxian Li <88318195+lijunxian111@users.noreply.github.com>
2026-02-03 20:22:25 +0800

    Update flux1_dev.py

--- model_eval/flux1_dev.py
+++ model_eval/flux1_dev.py
@@ -9,13 +9,12 @@ import torch.nn.functional as F
 original_scaled_dot_product_attention = F.scaled_dot_product_attention
 
 def patched_scaled_dot_product_attention(*args, **kwargs):
-    # 移除 enable_gqa 参数
     kwargs.pop('enable_gqa', None)
     return original_scaled_dot_product_attention(*args, **kwargs)
 
 F.scaled_dot_product_attention = patched_scaled_dot_product_attention
 
-pipe = FluxPipeline.from_pretrained("/data/pretrained/Flux/black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+pipe = FluxPipeline.from_pretrained("/path/to/Flux/black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
 pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
 
 def eval_flux1_dev(image, prompt, edit=False):
@@ -25,7 +24,7 @@ def eval_flux1_dev(image, prompt, edit=False):
         del pipe
         gc.collect()
         torch.cuda.empty_cache()
-        pipe = FluxKontextPipeline.from_pretrained("/data/pretrained/Flux1_Kontext_dev/", torch_dtype=torch.bfloat16)
+        pipe = FluxKontextPipeline.from_pretrained("/path/to/Flux1_Kontext_dev/", torch_dtype=torch.bfloat16)
         pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
         init_image = load_image(image)
         image = pipe(
@@ -49,17 +48,3 @@ def eval_flux1_dev(image, prompt, edit=False):
 
 if __name__ == "__main__":
     pass
-    
-"""
-prompt = "A cute cat."
-image = pipe(
-    prompt,
-    height=1024,
-    width=1024,
-    guidance_scale=3.5,
-    num_inference_steps=50,
-    max_sequence_length=512,
-    generator=torch.Generator("cpu").manual_seed(0)
-).images[0]
-image.save("flux-dev.png")
-"""
